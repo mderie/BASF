9bd9133b889c7d3aefa44b7f5433f577324de7f0
mderie <mderie@gmail.com>
2017-05-01 15:03:53 +0200

    nusb support

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -21,23 +21,9 @@ int matrix[8][8];
 LP* lp = NULL;
 bool done = false;
 
+typedef unsigned char[8] spectres
+
 using namespace std;
-
-
-// TIMER stuff ----------------------------------------------------------------
-
-class BPMTimer : public juce::HighResolutionTimer
-{
-public:
-	void hiResTimerCallback() override;
-};
-
-void BPMTimer::hiResTimerCallback()
-{
-	return;
-}
-
-BPMTimer* bpmTimer = NULL;
 
 // Midi callback stuff --------------------------------------------------------
 
@@ -102,11 +88,27 @@ void clearMatrix()
 	}
 }
 
-void displayMatrixOnLaunchpad()
+void displayMatrixOnLaunchpad(left, right spectres)
 {
 	// Clear it
+	lp->ClearScreen();
 
-	// Push the matrix content
+	// Push the matrix content... Really need to check the fast way of refreshing the entire LP
+	for (int j = 0; j < 4; j++)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			lp->SetPixel(i, j, ; // TO be continued...
+		}
+	}
+
+	for (int j = 5; j < 8; j++)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			lp->SetPixel(i, j, ;
+		}
+	}
 
 	return;
 }
@@ -128,122 +130,146 @@ void POC()
 // Code taken almost as is from JUCE play some file tutorial ------------------
 MainAudioComponent *mac = NULL;
 
-/*
-void stop()
+// For the POC : just wait a few seconds, fake the user input ------------------
+
+class BPMTimer : public juce::HighResolutionTimer
 {
-    changeState(Stopping);
-}
+public:
+	void hiResTimerCallback() override;
+};
 
-void play(const string& what)
+void BPMTimer::hiResTimerCallback()
 {
-    mac = new MainContentComponent();
+    done = true;
+    this->stopTimer();
+};
 
-    File file(what);
-    AudioFormatReader* reader = formatManager.createReaderFor (file);
+BPMTimer* bpmTimer = NULL;
 
-    if (reader != nullptr)
-    {
-        ScopedPointer<AudioFormatReaderSource> newSource = new AudioFormatReaderSource (reader, true);
-        transportSource.setSource (newSource, 0, nullptr, reader->sampleRate);
-        readerSource = newSource.release();
-    }
-
-	return;
-}
-*/
-
-//==============================================================================
+// The main (dish) ------------------------------------------------------------
 int main (int argc, char* argv[])
 {
 	cout << "Welcome to BASF (= Bare Audio Spectrogram Fourier)" << endl;
 
-	if (argc != 2)
+	if ((argc < 2) or (argc > 3))
 	{
 		cout << "Usage : " << endl;
-		cout << "basf audio_filename" << endl;
+		cout << "basf audio_filename [nousb]" << endl;
 		return -1;
 	}
 
+	bool nousb = false;
+	if (argc == 3)
+	{
+        if (strcmp(argv[2], "nousb") == 0)
+        {
+            nousb = true;
+        }
+        else
+        {
+            cout << "Usage : " << endl;
+            cout << "basf audio_filename [nousb]" << endl;
+            return -1;
+        }
+	}
+
+	juce::String fullFilename = File::getCurrentWorkingDirectory().getChildFile(argv[1]).getFullPathName();
+	//cout << "fullFilename = " << fullFilename << endl; // Care, strange construction to get path... Into non std string :(
+
     int i, lpOutIndex, lpInIndex;
+
 	LPCallback lpcb;
 	string dummy;
 
-	// Now get the LP for output
-
-    std::cout << "Names of all the midi output device (lp)" << std::endl;
-	juce::StringArray outputDevices = juce::MidiOutput::getDevices();
-	i = 0;
-	lpOutIndex = -1;
-	lpInIndex = -1;
-
-	// Microsoft specific ! https://msdn.microsoft.com/en-us/library/ms177202.aspx
-	// for each (juce::String item in outputDevices)
-    for (auto s : outputDevices)
+	if (!nousb)
 	{
-		if (s.contains("Launchpad"))
-		{
-			lpOutIndex = i;
-		}
+        // Now get the LP for output
+        std::cout << "Names of all the midi output device (lp)" << std::endl;
+        juce::StringArray outputDevices = juce::MidiOutput::getDevices();
+        i = 0;
+        lpOutIndex = -1;
+        lpInIndex = -1;
 
-		std::cout << "index " << i++ << " : " << s << std::endl;
+        // Microsoft specific ! https://msdn.microsoft.com/en-us/library/ms177202.aspx
+        // for each (juce::String item in outputDevices)
+        for (auto s : outputDevices)
+        {
+            if (s.contains("Launchpad"))
+            {
+                lpOutIndex = i;
+            }
+
+            std::cout << "index " << i++ << " : " << s << std::endl;
+        }
+
+        if (lpOutIndex == -1)
+        {
+            lpOutIndex = 1; // Fine tune this default value !
+        }
+
+        std::cout << "About to use " << lpOutIndex << " as index... Press ENTER or CTRL-C to stop !" << std::endl;
+        std::getline(std::cin, dummy);
+
+        midiLP = juce::MidiOutput::openDevice(lpOutIndex);
+        lp = new LP(midiLP);
+        lp->ClearScreen();
+
+        // Get again the LP but for input this time
+
+        std::cout << "Names of all the midi input device (lp again)" << std::endl;
+        juce::StringArray inputDevices = juce::MidiInput::getDevices();
+        i = 0;
+        for (auto s : inputDevices)
+        {
+            if (s.contains("Launchpad"))
+            {
+                lpInIndex = i;
+            }
+
+            std::cout << "index " << i++ << " : " << s << std::endl;
+        }
+
+        if (lpInIndex == -1)
+        {
+            lpInIndex = 1; // Fine tune this default value !
+        }
+
+        std::cout << "About to use" << lpInIndex << " as index... Press ENTER or CTRL-C to stop !" << std::endl;
+        std::getline(std::cin, dummy);
+
+        midiInput = juce::MidiInput::openDevice(lpInIndex, &lpcb);
+        midiInput->start();
 	}
 
-	if (lpOutIndex == -1)
-	{
-		lpOutIndex = 1; // Fine tune this default value !
-	}
-
-	std::cout << "About to use " << lpOutIndex << " as index... Press ENTER or CTRL-C to stop !" << std::endl;
-	std::getline(std::cin, dummy);
-
-	midiLP = juce::MidiOutput::openDevice(lpOutIndex);
-	lp = new LP(midiLP);
-	lp->ClearScreen();
-
-    // Get again the LP but for input this time
-
-	std::cout << "Names of all the midi input device (lp again)" << std::endl;
-	juce::StringArray inputDevices = juce::MidiInput::getDevices();
-	i = 0;
-	for (auto s : inputDevices)
-	{
-		if (s.contains("Launchpad"))
-		{
-			lpInIndex = i;
-		}
-
-		std::cout << "index " << i++ << " : " << s << std::endl;
-	}
-
-	if (lpInIndex == -1)
-	{
-		lpInIndex = 1; // Fine tune this default value !
-	}
-
-	std::cout << "About to use" << lpInIndex << " as index... Press ENTER or CTRL-C to stop !" << std::endl;
-	std::getline(std::cin, dummy);
-
-	midiInput = juce::MidiInput::openDevice(lpInIndex, &lpcb);
-	midiInput->start();
-
+	mac = new MainAudioComponent();
 	mac->play(argv[1]);
+	bpmTimer = new BPMTimer();
+	bpmTimer->startTimer(5000); // Time is money :)
 
 	// main loop :)
     while (!done)
 	{
 	}
 
-	// POC();
-
 	// Stop all !
+	delete bpmTimer;
+
 	mac->stop();
 	delete mac;
 
-	midiInput->stop();
-    delete midiInput;
+	if (!nousb)
+	{
+        midiInput->stop();
+        delete midiInput;
 
-	lp->ClearScreen();
-	delete lp;
+        //midiLP->stop();
+        delete midiLP;
+
+        lp->ClearScreen();
+        delete lp;
+    }
+
+    cout << "That's all folks !" << endl;
 
 	return 0;
 }
--- Source/MainAudioComponent.cpp
+++ Source/MainAudioComponent.cpp
@@ -1,23 +1,77 @@
 
+#include <iostream>
 #include "MainAudioComponent.hpp"
 
-MainAudioComponent::MainAudioComponent() : m_state(Stopped)
+//MainAudioComponent::MainAudioComponent() : m_state(Stopped)
+MainAudioComponent::MainAudioComponent() :
 {
-    m_formatManager.registerBasicFormats();
-    m_transportSource.addChangeListener (this);
-    setAudioChannels (0, 2);
+    m_afm.registerBasicFormats();
+    m_afrs = new AudioFormatReaderSource(m_afm.createReaderFor(input), true);
+    m_adm.initialiseWithDefaultDevices(0, 2);
+    m_aiod = m_adm.getCurrentAudioDevice();
+
+    if (!m_aiod)
+    {
+        std::cout << "Oups : m_aiod is null" << std::endl;
+    }
+
+    if (!m_afrs)
+    {
+        std::cout << "Oups : m_afrs is null" << std::endl;
+    }
+
+    m_asp = new AudioSourcePlayer();
+    if (!m_asp)
+    {
+        std::cout << "Oups : m_asp is null" << std::endl; // Very weird case :)
+    }
+    m_asp->setSource(m_afrs);
+
+    m_adm.addAudioCallback(m_asp);
+
+    //TODO : needed ?
+    //m_transportSource.addChangeListener();
+
+    // Code behind setAudioChannels(0, 2) in AudioAppComponent
+    //String audioError = m_deviceManager.initialise(0, 2, nullptr, true);
+    //jassert(audioError.isEmpty());
+    //m_deviceManager.addAudioCallback(&m_audioSourcePlayer);
+
+    //TODO : needed ?
+    //m_audioSourcePlayer.setSource(this);
 }
 
 MainAudioComponent::~MainAudioComponent()
 {
-    shutdownAudio();
+    if (m_afrs)
+    {
+        delete m_afrs;
+    }
+
+    // Needed ?
+    //if (m_aiod)
+    //{
+    //    delete m_aiod;
+    //}
+
+    if (m_asp)
+    {
+        delete m_asp;
+    }
+
+    m_adm.closeAudioDevice();
+
+    //TODO : Do we need to something about m_readerSource ? Not used to play with scoped ptr...
+
+    //m_transportSource.releaseResources();
+
+    // Code behind shutdownAudio() in AudioAppComponent
+    //m_audioSourcePlayer.setSource(nullptr);
+    //m_deviceManager.removeAudioCallback(&m_audioSourcePlayer);
+    //m_deviceManager.closeAudioDevice();
 }
 
-void MainAudioComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
-{
-    m_transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
-}
-
+/*
 void MainAudioComponent::getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill)
 {
     if (m_readerSource == nullptr)
@@ -26,15 +80,12 @@ void MainAudioComponent::getNextAudioBlock(const AudioSourceChannelInfo& bufferT
         return;
     }
 
-    m_transportSource.getNextAudioBlock (bufferToFill);
+    m_transportSource.getNextAudioBlock(bufferToFill);
 }
+*/
 
-void MainAudioComponent::releaseResources()
-{
-    m_transportSource.releaseResources();
-}
-
-void MainAudioComponent::changeListenerCallback (ChangeBroadcaster* source)
+/*
+void MainAudioComponent::changeListenerCallback(ChangeBroadcaster* source)
 {
     if (source == &m_transportSource)
     {
@@ -44,11 +95,13 @@ void MainAudioComponent::changeListenerCallback (ChangeBroadcaster* source)
         }
         else
         {
-            changeState(Stopped);
+            changeState(Stopped); // Or Stopping ?
         }
     }
 }
+*/
 
+/*
 void MainAudioComponent::changeState(TransportState newState)
 {
     if (m_state != newState)
@@ -57,38 +110,62 @@ void MainAudioComponent::changeState(TransportState newState)
 
         switch (m_state)
         {
-            case Stopped:
-                m_transportSource.setPosition(0.0);
-                break;
-
             case Starting:
+                std::cout << "state = Starting" << std::endl;
                 m_transportSource.start();
                 break;
 
             case Playing:
+                std::cout << "state = Playing" << std::endl;
                 break;
 
             case Stopping:
+                std::cout << "state = Stopping" << std::endl;
                 m_transportSource.stop();
+                break;
+
+            case Stopped:
+                std::cout << "state = Stopped" << std::endl;
+                m_transportSource.setPosition(0.0);
                 break;
         }
     }
 }
+*/
 
 void MainAudioComponent::play(const std::string& what)
 {
-    File file(what);
-    AudioFormatReader* reader = m_formatManager.createReaderFor (file);
+    File file(File::getCurrentWorkingDirectory().getChildFile(juce::String(what)));
+    std::cout << "file.getFullPathName() = " << file.getFullPathName() << std::endl;
+
+	while (m_afrs->getNextReadPosition() < m_afrs->getTotalLength())
+	{
+		Thread::sleep(100);
+	}
+}
+
+/*
+void MainAudioComponent::play(const std::string& what)
+{
+    File file(File::getCurrentWorkingDirectory().getChildFile(juce::String(what)));
+    std::cout << "file.getFullPathName() = " << file.getFullPathName() << std::endl;
+    AudioFormatReader* reader = m_formatManager.createReaderFor(file);
 
     if (reader != nullptr)
     {
         ScopedPointer<AudioFormatReaderSource> newSource = new AudioFormatReaderSource(reader, true);
-        m_transportSource.setSource (newSource, 0, nullptr, reader->sampleRate);
+        m_transportSource.setSource(newSource, 0, nullptr, reader->sampleRate);
         m_readerSource = newSource.release();
     }
-}
 
+    changeState(Starting);
+    m_transportSource.prepareToPlay(512, 44100); // Check the validity of the first parameter !
+}
+*/
+
+/*
 void MainAudioComponent::stop()
 {
     changeState(Stopping);
 }
+*/
